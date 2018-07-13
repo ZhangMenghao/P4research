ac24be76d524a4bd32abcdfec81a3a939be18116
kongxiao0532 <kongxiao0532@163.com>
2018-07-13 17:20:15 +0800

    preparing for INFOCOM. mark. Notice that when testing SYN proxy, connection has to be initiated by h2 with h1 being the server, or it will not work.

--- DoS/bmv2/run_switch.py
+++ DoS/bmv2/run_switch.py
@@ -16,7 +16,7 @@ proxy_on_action_name = 'turn_on_proxy'
 proxy_off_action_name = 'turn_off_proxy'
 blacklist_register_name = 'blacklist_table'
 whitelist_register_name = 'whitelist_table'
-proxy_status = 1
+proxy_status = -1
 
 def send_to_CLI(cmd):
     this_dir = os.path.dirname(os.path.realpath(__file__))
@@ -69,32 +69,41 @@ def read_counters():
 
 def turn_on_proxy():
     global proxy_status
+    if proxy_status == 1:
+        return
+    print 'Turning on proxy...'
     print table_reset_default(proxy_status_table_name)
     print table_set_default(proxy_status_table_name, proxy_on_action_name)
     proxy_status = 1
 
 def turn_off_proxy():
     global proxy_status
+    if proxy_status == 0:
+        return
+    print 'Turning off proxy...'
     print table_reset_default(proxy_status_table_name)
     print table_set_default(proxy_status_table_name, proxy_off_action_name)
     proxy_status = 0
     
 def check_syn_and_ack_number(listen_interval, last_counter_val, syn_packets_speed_threshold=100):
-    print last_counter_val
+    print 'last_counter_val:', last_counter_val
     # meter_result = read_meter()
     counter_results = read_counters()
-    print counter_results
+    print 'counter_results:', counter_results
+    if last_counter_val[0] == -1 and last_counter_val[1] == -1:
+        # do not calculate spped
+        return [counter_results['syn'][0], counter_results['vack'][0]]
     # syn speed
     syn_speed = float((counter_results['syn'][0] - last_counter_val[0]) / listen_interval)
     if syn_speed > syn_packets_speed_threshold:
         print 'Syn Proxy On. \tSpeed of syn packets is %d.' % syn_speed
         turn_on_proxy()
     # number of syn & valid ack packets
-    print proxy_status
+    print 'proxy_status:', proxy_status
     if proxy_status == 1:
         syn_increase = counter_results['syn'][0] - last_counter_val[0]
         vack_increase = counter_results['vack'][0] - last_counter_val[1]
-        print syn_increase, vack_increase
+        print 'syn_increase:', syn_increase, 'vack_increase:', vack_increase
         if abs(syn_increase - vack_increase) < min(syn_increase, vack_increase) * 1 / 8.0:
             print 'Syn Proxy Off. \tDifferece between syn and valid ack packets during the last period is %d.' % abs(syn_increase - vack_increase)
             turn_off_proxy()
@@ -102,7 +111,7 @@ def check_syn_and_ack_number(listen_interval, last_counter_val, syn_packets_spee
     return [counter_results['syn'][0], counter_results['vack'][0]]
 
 def update_black_list(rows=4096):
-    blacklist_result = 0 * rows
+    blacklist_result = [0] * rows
     for i in range(0, rows):
         register_result = register_read(blacklist_register_name, i)
         pattern = re.compile(blacklist_register_name + r'\[\d+\]=\s*(\d+)')
@@ -115,27 +124,30 @@ def update_black_list(rows=4096):
                 register_write(blacklist_register_name, i, 0)
 
 def update_white_list(rows=4096):
-    blacklist_result = 0 * rows
+    whitelist_result = [0] * rows
     for i in range(0, rows):
         register_result = register_read(whitelist_register_name, i)
-        pattern = re.compile(blacklist_register_name + r'\[\d+\]=\s*(\d+)')
+        pattern = re.compile(whitelist_register_name + r'\[\d+\]=\s*(\d+)')
         match = pattern.search(register_result)
         if(match):
-            blacklist_result[i] = match.group(1)
-            if blacklist_result[i] >= 2: # 10 or 11
-                register_write(blacklist_register_name, i, 1)
+            whitelist_result[i] = match.group(1)
+            if whitelist_result[i] >= 2: # 10 or 11
+                register_write(whitelist_register_name, i, 1)
             else:
-                register_write(blacklist_register_name, i, 0)
+                register_write(whitelist_register_name, i, 0)
 def main():
     global proxy_status
-    listen_interval = 0.5
-    last_counter_val = [0, 0]
+    listen_interval = 0.1
+    last_counter_val = [-1, -1]
     while True:
         last_counter_val = check_syn_and_ack_number(listen_interval, last_counter_val)
-        update_black_list()
-        update_white_list()
+        # it takes about 2.5 sec to check 10 entries......
+        # update_black_list(10)
+        # update_white_list(10)
+        print '\n'
         time.sleep(listen_interval)
     
 
 if __name__ == '__main__':
     main()
+
